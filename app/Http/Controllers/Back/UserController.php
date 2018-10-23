d738a906eecb7bd971630bd2095bc2447f36dc4a
hell0x <327466897@qq.com>
2018-10-23 17:19:15 +0800

    role user permission

---
+++ app/Http/Controllers/Back/UserController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by weixing.
+ * User: weixing
+ * Date: 2018/10/23
+ * Time: 14:54
+ */
+
+namespace App\Http\Controllers\Back;
+
+use Illuminate\Http\Request;
+
+class UserController
+{
+
+    protected $userRepository;
+
+    protected $nbrPages;
+
+
+
+
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by weixing.
+ * User: weixing
+ * Date: 2018/10/23
+ * Time: 17:17
+ */
+
+namespace App\Repositories;
+
+
+class UserRepository
+{
+
+}
